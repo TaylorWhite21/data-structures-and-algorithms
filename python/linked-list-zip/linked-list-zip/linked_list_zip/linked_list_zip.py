488ede4f1453f48fa774be94577ece8d9e9ef7b2
Taylor <taylorwhite21@yahoo.com>
2022-01-06 17:26:55 -0500

    test commit

--- python/linked-list-zip/linked-list-zip/linked_list_zip/linked_list_zip.py
+++ python/linked-list-zip/linked-list-zip/linked_list_zip/linked_list_zip.py
@@ -1,20 +1,33 @@
 class LinkedList:
   def __init__(self, head=None):
     self.head = head
-  
-  def  merge_linked_lists(list_1, list_2):
-    new_list = Node()
-    tail = new_list
     
-    while list_1 != None and list_2 != None:
-      tail.next = list_1
-      tail = list_1
-      list_1 = list_1.next
-      
-      tail.next = list_2
-      tail = list_2
-      list_2 = list_2.next
-    return new_list.next
+  # Got help from Marie Marcos(TA) and followed along with Roger
+  
+  def append(self, value):
+    current = self.head
+    node = Node(value)
+    while current:
+      if current.next == None:
+        node.next = None
+        current.next = node
+  
+  # @staticmethod
+  def merge_linked_lists(self, list_1, list_2):
+    ll_3 = LinkedList()
+    current1 = list_1.head
+    current2 = list_2.head
+    
+    while current1 or current2:
+      if current1:
+        ll_3.append(current1)
+        current1 = current1.next
+        
+      if current2:
+        ll_3.append(current2.value)
+        current2 = current2.next
+
+    return ll_3
 
 class Node:
   # Default next to none so that you dont have to worry about ending the list
